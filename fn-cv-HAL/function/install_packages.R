1af430c86c675695bcc7aee7d16eb931fe023d47
Hugh Sturrock <hughsturrock@gmail.com>
2019-06-18 21:28:52 -0700

    Updating install_packages.r

--- fn-cv-HAL/function/install_packages.R
+++ fn-cv-HAL/function/install_packages.R
@@ -1,5 +1,7 @@
 install.packages(c('jsonlite',
                  'origami',
-                 'parallel'))
+                 'parallel',
+                 'devtools'))
 
+library(devtools)
 devtools::install_github("tlverse/hal9001", build_vignettes = FALSE)
