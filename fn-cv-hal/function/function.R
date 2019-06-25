0735af9834b2258e863fb25584a0cf083ffd5326
Hugh Sturrock <hughsturrock@gmail.com>
2019-06-25 08:33:45 -0700

    Adding random forest option

--- fn-cv-hal/function/function.R
+++ fn-cv-hal/function/function.R
@@ -9,6 +9,7 @@ function(params) {
   # run function and catch result
   points <- params[['points']]
   layer_names <- params[['layer_names']]
+  model_type <- "randomforest"
 
   points_df <- as.data.frame(points)
   points_df$n_negative <- points_df$n_trials - points_df$n_positive
@@ -22,6 +23,10 @@ function(params) {
   folds_list <- origami::make_folds(points_df_train)
   folds_df_list <- lapply(folds_list, folds_list_to_df_list, df = points_df_train)
   
+  # Save validation indeces for later
+  valid_indeces <- unlist(sapply(folds_list, function(x){x$validation_set}))
+  
+  if(model_type == "hal"){
   # Now apply HAL to each fold in parallel 
   cv_predictions <- parallel::mclapply(folds_df_list, FUN = fit_hal_parallel,
                              mc.cores = detectCores() - 1,
@@ -30,17 +35,47 @@ function(params) {
                              n_neg_var = "n_negative")
   
   # Add cv predictions back onto data.frame
-  valid_indeces <- unlist(sapply(folds_list, function(x){x$validation_set}))
   points_df_train$cv_preds[valid_indeces] <- unlist(cv_predictions)
   
   # Now fit HAL to full dataset and create fitted predictions
   hal_fit <- fit_hal(X = points_df_train[,layer_names], 
                      Y = cbind(points_df_train$n_negative,
                                points_df_train$n_positive), 
-                     family = "binomial")
+                     family = "binomial", yolo = FALSE)
   points$fitted_predictions <- predict(hal_fit, new_data = points_df[,layer_names])
   points$cv_predictions <- NA
   points$cv_predictions[points_df_train$row_id[valid_indeces]] <- unlist(cv_predictions)
-
+  }
+  
+  
+  if(model_type == "randomforest"){
+    
+    cv_predictions <- parallel::mclapply(folds_df_list, FUN = fit_rf_parallel,
+                                         mc.cores = detectCores() - 1,
+                                         X_var = layer_names,
+                                         n_pos_var = "n_positive",
+                                         n_neg_var = "n_negative")
+    
+    # Add cv predictions back onto data.frame
+    points_df_train$cv_preds[valid_indeces] <- unlist(cv_predictions)
+    
+    # Now fit RF to full dataset and create fitted predictions
+    Y <- factor(c(rep(0, nrow(points_df_train)),
+                  rep(1, nrow(points_df_train))))
+    
+    X <- points_df_train[,layer_names]
+    X <- rbind(X, X)
+    rf_formula <- as.formula(paste("Y", "~", paste(layer_names, collapse = "+")))
+    rf_fit <- ranger(rf_formula,
+                     data = points_df_train,
+                     probability = TRUE,
+                     case.weights = c(points_df_train$n_negative,
+                                      points_df_train$n_positive))
+    
+    fitted_predictions <- predict(rf_fit, points_df[,layer_names])
+    points$fitted_predictions <- fitted_predictions$predictions[,2]
+    points$cv_predictions <- NA
+    points$cv_predictions[points_df_train$row_id[valid_indeces]] <- unlist(cv_predictions)
+  }
   return(points)
 }
--- fn-cv-hal/function/helpers.R
+++ fn-cv-hal/function/helpers.R
@@ -1,3 +1,5 @@
+library(hal9001)
+library(ranger)
 
 # Helpers for cv-hal
 
@@ -19,6 +21,30 @@ fit_hal_parallel <- function(folds_df_list_fold,
              folds_df_list_fold$train[,n_pos_var])
   pred_data <- folds_df_list_fold$valid[,X_var]
   
-  hal_mod <- fit_hal(X, Y, family = "binomial")
+  hal_mod <- fit_hal(X, Y, family = "binomial", yolo = FALSE)
   predict(hal_mod, new_data = pred_data)
+}
+
+fit_rf_parallel <- function(folds_df_list_fold, 
+                             X_var,
+                             n_pos_var,
+                             n_neg_var){
+  
+  # Reshape into 0 and 1 rows per observation
+  Y <- factor(c(rep(0, nrow(folds_df_list_fold$train)),
+         rep(1, nrow(folds_df_list_fold$train))))
+  
+  X <- folds_df_list_fold$train[,X_var]
+  X <- rbind(X, X)
+  mod_data <- cbind(Y, X)
+  pred_data <- folds_df_list_fold$valid[,X_var]
+  
+  rf_formula <- as.formula(paste("Y", "~", paste(X_var, collapse = "+")))
+  rf_mod <- ranger(rf_formula,
+                          data = mod_data,
+                          probability = TRUE,
+                          case.weights = c(folds_df_list_fold$train$n_negative,
+                                      folds_df_list_fold$train$n_positive))
+  preds <- predict(rf_mod, pred_data, type = "response")
+  return(preds$predictions[,2])
 }
